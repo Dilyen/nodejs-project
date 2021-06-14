3694aec9b19e5f38bb7fa5f4e0c40ee5edb0a1eb
Dilyen <davidandohacquah@gmail.com>
2021-06-14 09:21:27 +0000

    starting my first node poject

---
+++ play.js
@@ -0,0 +1,52 @@
+/// VAR LET AND CONST ///
+// const name = 'David Andoh-Acuah';
+// let age = 23;
+// const HasHobbies = true;
+
+// age = 30;
+
+/// ERROR FUNCTION 
+// function summerizeUser = (userName, userAge, userHasHobbies) => {
+//     this  
+//     return('My name is ' + 
+//     userName + 
+//     ' and my age is ' + 
+//     userAge + 
+//     ' years of age ' + 
+//     ' due to that l have hobbies: ' +
+//      userHasHobbies)
+//      ;
+// }
+
+// console.log(summerizeUser(name, age, HasHobbies));
+
+/// WORKING WITH OBJECTS, PROPERTIES AND ETHODS 
+// const person = {
+//     name: 'Dav',
+//     age: 39,
+//     greet() {
+//         console.log('Hi there, I am ' + this.name)
+//     }
+// }
+
+// person.greet();
+
+/// ///ARRAR & ARRAY METHODS //////
+// const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies){
+//     console.log(hobby);
+
+// }
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+/// ARRAY OBJECT AND REFERENCE TYPES ///
+// const hobbies = ['Sports', 'Cooking'];
+// hobbies.push('Cooking', 'Programming');
+// console.log(hobbies);
+
+//////// UNDERSTANDING SPREAD & REST OPERATORS ///////
+const copiedArray = [...hobbies];
+console
+
+
