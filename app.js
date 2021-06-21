263024bd4ef31db95104d998c468fcfb48ba157d
Dilyen <davidandohacquah@gmail.com>
2021-06-21 17:16:09 +0000

    implimented handlebars templating engine and aedded a layout to handlebars

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expresshbs  = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+
+app.engine(
+    'hbs', 
+    expresshbs({
+        layoutsDir: 'views/layouts/', 
+        defaultLayout: 'main-layout', 
+        extname: 'hbs'
+    })
+        );
+app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');for pug template implimentation//
 app.set('views', 'views'); 
 
 const adminData = require('./routes/admin');
@@ -21,5 +31,5 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
-
+ 
 app.listen(2000);
