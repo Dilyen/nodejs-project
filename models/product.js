2bd2d67b92dec387f303cec5d454e022acf7892b
Dilyen <davidandohacquah@gmail.com>
2021-06-29 08:42:47 +0000

    implimented a helper function in the product.js modal

--- models/product.js
+++ models/product.js
@@ -1,15 +1,37 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(JSON.stringify(fileContent)));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t){
-        this.title = t;
-    }
-    save() {
-        products.push(this);
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    static fetchAll() {
-        return products;
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
